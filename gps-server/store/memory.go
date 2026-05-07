bffde186d870555b71736923ef5ccb44d8ef4517
jeganxthan <jegan4044@gmail.com>
2026-05-07 12:54:20 +0530

    device fixed

--- gps-server/store/memory.go
+++ gps-server/store/memory.go
@@ -4,6 +4,7 @@ import "gps-server/models"
 
 var Devices = map[string]models.Device{
 	"359339075496001": {IMEI: "359339075496001", Name: "Test Device"},
+	"354778340715856": {IMEI: "354778340715856", Name: "Jegan's Device"},
 }
 
 func GetDevice(imei string) (models.Device, bool) {
